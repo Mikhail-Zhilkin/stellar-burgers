0e5a134b63cda5ea94bb01ec4094f4305bf8d684
Mikhail Zhilkin <mikhailzhilkin@yandex.ru>
2025-05-12 17:03:29 +0300

    Готовая проектная работа

---
+++ src/components/protected-route/protected-route.tsx
@@ -0,0 +1,31 @@
+import { Navigate, useLocation } from 'react-router-dom';
+import { useSelector } from '../../services/store';
+
+type ProtectedRouteProps = {
+  onlyUnAuth?: boolean;
+  children: React.ReactElement;
+};
+
+export const ProtectedRoute = ({
+  onlyUnAuth,
+  children
+}: ProtectedRouteProps) => {
+  const user = useSelector((state) => state.userSlice.data);
+  const location = useLocation();
+
+  //   if (!isAuthChecked) {
+  //     return <Preloader />;
+  //   }
+
+  if (!onlyUnAuth && !user) {
+    return <Navigate replace to='/login' state={{ from: location }} />;
+  }
+
+  if (onlyUnAuth && user) {
+    const from = location.state?.from || { pathname: '/' };
+
+    return <Navigate replace to={from} />;
+  }
+
+  return children;
+};
